f25d83ffdc6d641c55d7faba79011c5eb9b1b5e0
YasseenR <yasseenr@gmail.com>
2024-06-13 10:46:56 -0400

    Initial Commit

---
+++ FaceFacts/FaceFactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceFactsApp.swift
+//  FaceFacts
+//
+//  Created by Yasseen Rouni on 6/13/24.
+//
+
+import SwiftUI
+
+@main
+struct FaceFactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
